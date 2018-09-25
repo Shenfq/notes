88231584c3085edb664d7e1526bfe047cfe72ce8
Shenfq <shenfq95@foxmail.com>
2018-09-25 13:05:27 +0800

    feat: add travis :rocket:

--- gh-pages.js
+++ gh-pages.js
@@ -1,4 +1,9 @@
+const fs = require('fs')
+const path = require('path')
 const ghpages = require('gh-pages')
+
+fs.writeFileSync(path.resolve(__dirname, ".vuepress/dist/CNAME"), 'notes.shenfq.com');
+
 ghpages.publish('./.vuepress/dist', {
   branch: 'gh-pages',
   repo: 'git@github.com:Shenfq/notes.git'
