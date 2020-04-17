416c629a2fe2c6dc67adf29ae1d2b0653689a947
Shenfq <shenfq95@foxmail.com>
2020-04-17 11:33:28 +0800

    feat: 添加百度统计

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,16 @@ module.exports = {
   description: '自然醒的学习笔记',
   search: false,
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['script', {}, `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?b887a437433180b7cdce3335aedd741d";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+    `]
   ],
   plugins: [
     ['image']
